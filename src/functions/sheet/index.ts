59c1ee516d4703004616a8d04c67ed1a7e2a65a1
jroehl <mail@johannroehl.de>
2021-07-09 06:55:20 +0200

    fix: longer timeout

--- src/functions/sheet/index.ts
+++ src/functions/sheet/index.ts
@@ -3,6 +3,7 @@ import schema from '../schema';
 
 export default {
   handler: `${__dirname.split(process.cwd())[1].substring(1)}/handler.main`,
+  timeout: 30, // timeout 30 seconds
   events: [
     {
       http: {
