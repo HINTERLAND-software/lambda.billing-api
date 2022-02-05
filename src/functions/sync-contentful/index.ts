45ce85ed09fd185a5b4e8c2c03936b80ae1b67e6
jroehl <mail@johannroehl.de>
2022-02-05 15:15:16 +0100

    chore: Increase timeout for sync-contentful

--- src/functions/sync-contentful/index.ts
+++ src/functions/sync-contentful/index.ts
@@ -2,7 +2,7 @@ import type { AWS } from '@serverless/typescript';
 
 export default {
   handler: `${__dirname.split(process.cwd())[1].substring(1)}/handler.main`,
-  timeout: 6, // timeout 6 seconds
+  timeout: 30, // timeout 6 seconds
   events: [
     {
       http: {
